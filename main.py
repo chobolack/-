2fe025adc05a9ad7c7d1bf8755341c2c99df57f2
chobolack <98378211+chobolack@users.noreply.github.com>
2023-12-25 06:27:14 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import math
 import function
 import matplotlib.pyplot as plt
 
@@ -29,16 +28,16 @@ Line_OA = 2508
 Line_AB = 2000
 Line_CO = 3000
 Line_BC = 2700
-Line_DC = Line_BC / 2
-Line_CE = 1000
+Line_DC = 1350
+Line_CE = 2000
 
 # 鋼管參數
-Pipe_width = 120
-Pipe_height = 120
-Pipe_thickness = 4
+Pipe_width = 100
+Pipe_height = 150
+Pipe_thickness = 5
 
 # 鋼管材質的降伏強度
-Syt_Pipe = 900
+Syt_Pipe = 490
 
 # 銷材質的降伏強度
 Syt_Pin = 765
@@ -46,8 +45,8 @@ Syt_Pin = 765
 # 接頭寬
 Clevis_width = 81
 
-#焊接
-
+# 焊接材料的抗拉強度
+Sut_welding = 638
 #===================================================以下為函式呼叫===================================================
 
 # 抬升前
@@ -59,9 +58,13 @@ pipe = function.Pipe(Pipe_height, Pipe_width, Pipe_thickness)
 # 拉桿安全係數
 Sf = function.SafetyFactor(Syt_Pipe, pipe[1], after[5], Pipe_height)
 # 上拉桿最大應力點
-abc = function.abc(after[7], Pipe_width, pipe[1], after[1], after[8], pipe[0])
+MaxPoint = function.MaxPoint(after[7], Pipe_width, pipe[1], after[1], after[8], pipe[0])
 # 銷直徑最小值
 Pin = function.Pin(after[0], Clevis_width, Syt_Pin)
+# 上焊接
+Tau_Up = function.Tau_Up(after[0], after[9], Pipe_height, Pipe_width, Pipe_height, Pipe_thickness, Sut_welding)
+# 下焊接
+Tau_Down = function.Tau_Down(after[0], Pipe_height, Pipe_width, Pipe_height, Pipe_thickness, Sut_welding)
 
 #===================================================以下為數據輸出===================================================
 
@@ -126,15 +129,15 @@ print("")
 print("    上拉桿最大應力點:")
 print("")
 print("    Bending:")
-print(f"    σmax = {round(abc[0], num)} MPa")
+print(f"    σmax = {round(MaxPoint[0], num)} MPa")
 print("")
 print("    Axial load:")
-print(f"    σ = {round(abc[1], num)} MPa")
+print(f"    σ = {round(MaxPoint[1], num)} MPa")
 print("")
-print(f"    σ = σmax + σ = {round(abc[2], num)} MPa")
+print(f"    σ = σmax + σ = {round(MaxPoint[2], num)} MPa")
 print("")
 print(f"    σallow = {Syt_Pipe / 2} MPa")
-if (Syt_Pipe / 2) > abc[2]:
+if (Syt_Pipe / 2) > MaxPoint[2]:
     print("    σallow > σ")
     print(f"    {green}result pass {check_mark}{color_end}")
 else:
@@ -148,11 +151,39 @@ print("")
 print(f"    Pin的最小直徑 d = {round(Pin, num)} mm")
 
 print("--------------------------------------------")
+
+print("    焊接分析:")
+print("")
+print(f"    τallow = {round(Tau_Up[2], num)} MPa")
+print("")
+print("    上拉桿焊接:")
+print(f"    τ = {round(Tau_Up[0], num)} MPa")
+if Tau_Up[1] == True:
+    print("    τallow > τ")
+    print(f"    {green}result pass {check_mark}{color_end}")
+else:
+    print("    τallow < τ")
+    print(f"    {red}result failed {failed_mark}{color_end}")
+print("")
+print("    下拉桿焊接:")
+print(f"    τ = {round(Tau_Down[0], num)} MPa")
+if Tau_Down[1] == True:
+    print("    τallow > τ")
+    print(f"    {green}result pass {check_mark}{color_end}")
+else:
+    print("    τallow < τ")
+    print(f"    {red}result failed {failed_mark}{color_end}")
+
+print("--------------------------------------------")
 # ===================================================以下為檢測數據===================================================
 
 function.check_debug_before(num)
 print("")
 function.check_debug_after(num)
+print("")
+function.check_debug_Up(9)
+print("")
+function.check_debug_Down(9)
 
 # ===================================================以下為剪力繪圖===================================================
 
